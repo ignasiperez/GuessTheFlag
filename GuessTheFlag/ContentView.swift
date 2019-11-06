6b309f937d2f816559ee51f152b1e77c22f8a690
Ignasi Perez <perezignasi@gmail.com>
2019-11-06 19:15:11 +0100

    D21-22-Project2 - Guess the Flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -26,18 +26,32 @@ struct ContentView: View {
   @State private var showingScore = false
   @State private var scoreTitle = ""
   
+  @State private var score = 0
+  
+  
   var body: some View {
     ZStack {
-      Color(red: 31 / 255,
-            green: 56 / 255,
-            blue: 100 / 255)
+      //      Color(red: 31 / 255,
+      //            green: 56 / 255,
+      //            blue: 100 / 255)
+      //        .edgesIgnoringSafeArea(.all)
+      LinearGradient(
+        gradient: Gradient(colors: [Color(red: 31 / 255,
+                                          green: 56 / 255,
+                                          blue: 100 / 255),
+                                    .black]),
+        startPoint: .top,
+        endPoint: .bottom)
         .edgesIgnoringSafeArea(.all)
-      VStack(spacing: 30) {
+      VStack(spacing: 50) {
         VStack {
           Text("Tap the flag of")
             .foregroundColor(.white)
+            .font(.title)
           Text(countries[correctAnswer])
             .foregroundColor(.white)
+            .font(.largeTitle)
+            .fontWeight(.bold)
         }
         
         ForEach(0 ..< 3) { number in
@@ -47,12 +61,19 @@ struct ContentView: View {
             Image(self.countries[number])
               .renderingMode(.original)
           }
+          .clipShape(Capsule())
+          .overlay(Capsule().stroke(
+            Color.black,
+            lineWidth: 1))
+            .shadow(color: .gray, radius: 10)
         }
+        Text("Your score is \(score) points")
+          .foregroundColor(.white).font(.title)
         Spacer()
       }
     }.alert(isPresented: $showingScore) {
       Alert(title: Text(scoreTitle),
-            message: Text("Your score is ???"),
+            message: Text("Your score is \(score) points"),
             dismissButton: .default(Text("Continue")) {
               self.askQuestion()
         })
@@ -62,10 +83,9 @@ struct ContentView: View {
   func flagTapped(_ number: Int) {
     if number == correctAnswer {
       scoreTitle = "Correct"
-      print("flagTapped - Correct")
+      score += 1
     } else {
-      scoreTitle = "Wrong"
-      print("flagTapped - Wrong")
+      scoreTitle = "Wrong! That’s the flag of \(countries[number])"
     }
     
     showingScore = true
